795e2bbe3c9cc46460f7b948788b6f48ca778621
Adrian Sampson <adrian@radbox.org>
2014-08-21 15:23:05 -0700

    Add header file

--- snnap.c
+++ snnap.c
@@ -1,4 +1,4 @@
-#include <stdbool.h>
+#include "snnap.h"
 #include <assert.h>
 
 static const unsigned NBUFS = 2;  // Number of input & output buffers.
@@ -25,8 +25,6 @@ static void wfe() {
     __asm__ __volatile__ ("WFE\n");
 }
 
-/* Initialize the library's state.
- */
 void snnap_init() {
     ibn = 0;
     obn = 0;
@@ -37,46 +35,19 @@ void snnap_init() {
     }
 }
 
-/*
-Two possible reading strategies. First is a "try-read":
 
-    if (snnap_canread()) {
-        char *buf = snnap_readbuf();  // Must succeed.
-        do_something(buf);
-        snnap_consumebuf();
-    }
-
-Second is a "blocking read":
-
-    snnap_block();
-    char *buf = snnap_readbuf();  // Must succeed.
-    do_something(buf);
-    snnap_consumebuf();
-
-*/
-
-/* Check whether the current output buffer is full, i.e., whether it is safe
- * to call `snnap_readbuf()`.
- */
 bool snnap_canread() {
     assert(ibf[obn]);
     assert(invoked[obn]);
     return obf[obn];
 }
 
-/* Get the most recent output data from the NPU. Precondition: the current
- * output buffer must be full.
- */
 const volatile char *snnap_readbuf() {
     assert(invoked[obn]);
     assert(obf[obn]);
     return obuf[obn];
 }
 
-/* "Free" the current output buffer to indicate that it can be filled by
- * another invocation. Precondition: the buffer must be full (i.e., don't call
- * this when the buffer is already free).
- */
 void snnap_consumebuf() {
     assert(obf[obn]);
     assert(ibf[obn]);
@@ -87,11 +58,6 @@ void snnap_consumebuf() {
     INCR_BUF(obn);
 }
 
-/* Wait until the current output buffer has data ready. Precondition: the
- * corresponding input buffer is full, but the output buffer itself may or may
- * not be full (i.e., the work has been submitted but may or may not have
- * completed). Postcondition: the output buffer is full.
- */
 void snnap_block() {
     assert(ibf[obn]);
     assert(invoked[obn]);
@@ -100,41 +66,12 @@ void snnap_block() {
     }
 }
 
-/* Correspondingly, two ways to write:
-
-A "try-write":
-
-    if (snnap_canwrite()) {
-        *(snnap_writebuf()) = data;
-        snnap_sendbuf();
-    }
-
-A "blocking write" may need to read before it can write:
-
-    if (!snnap_canwrite()) {
-        snnap_block();
-        char *buf = snnap_readbuf();
-        do_something(buf);
-        snnap_consumebuf();
-    }
-    assert(snnap_canwrite());  // Must succeed now.
-    *(snnap_writebuf()) = data;
-    snnap_sendbuf();
-
-*/
-
-/* Check whether it is safe to call `snnap_writebuf()`, i.e., there is an open
- * slot for new inputs to the NPU (the current input buffer is not full).
- */
 bool snnap_canwrite() {
     // This is only false when we've come "around the loop" back to a buffer
     // that has been submitted but not consumed.
     return !ibf[ibn];
 }
 
-/* Get a pointer to the buffer where inputs should be written. Precondition:
- * the current input buffer is not full.
- */
 volatile char *snnap_writebuf() {
     assert(!ibf[ibn]);
     assert(!obf[ibn]);
@@ -143,10 +80,6 @@ volatile char *snnap_writebuf() {
     return ibuf[ibn];
 }
 
-/* Invoke the NPU on the most recently written NPU. Move to the next input
- * buffer. Precondition: the current write buffer is full but has not been
- * invoked yet.
- */
 void snnap_sendbuf() {
     assert(ibf[ibn]);
     assert(!obf[ibn]);
---
+++ snnap.h
@@ -0,0 +1,90 @@
+#ifndef __SNAP_H
+#define __SNAP_H
+
+#include <stdbool.h>
+
+/* Initialize the library's state.
+ */
+void snnap_init();
+
+/*
+Two possible reading strategies. First is a "try-read":
+
+    if (snnap_canread()) {
+        char *buf = snnap_readbuf();  // Must succeed.
+        do_something(buf);
+        snnap_consumebuf();
+    }
+
+Second is a "blocking read":
+
+    snnap_block();
+    char *buf = snnap_readbuf();  // Must succeed.
+    do_something(buf);
+    snnap_consumebuf();
+
+*/
+
+/* Check whether the current output buffer is full, i.e., whether it is safe
+ * to call `snnap_readbuf()`.
+ */
+bool snnap_canread();
+
+/* Get the most recent output data from the NPU. Precondition: the current
+ * output buffer must be full.
+ */
+const volatile char *snnap_readbuf();
+
+/* "Free" the current output buffer to indicate that it can be filled by
+ * another invocation. Precondition: the buffer must be full (i.e., don't call
+ * this when the buffer is already free).
+ */
+void snnap_consumebuf();
+
+/* Wait until the current output buffer has data ready. Precondition: the
+ * corresponding input buffer is full, but the output buffer itself may or may
+ * not be full (i.e., the work has been submitted but may or may not have
+ * completed). Postcondition: the output buffer is full.
+ */
+void snnap_block();
+
+/* Correspondingly, two ways to write:
+
+A "try-write":
+
+    if (snnap_canwrite()) {
+        *(snnap_writebuf()) = data;
+        snnap_sendbuf();
+    }
+
+A "blocking write" may need to read before it can write:
+
+    if (!snnap_canwrite()) {
+        snnap_block();
+        char *buf = snnap_readbuf();
+        do_something(buf);
+        snnap_consumebuf();
+    }
+    assert(snnap_canwrite());  // Must succeed now.
+    *(snnap_writebuf()) = data;
+    snnap_sendbuf();
+
+*/
+
+/* Check whether it is safe to call `snnap_writebuf()`, i.e., there is an open
+ * slot for new inputs to the NPU (the current input buffer is not full).
+ */
+bool snnap_canwrite();
+
+/* Get a pointer to the buffer where inputs should be written. Precondition:
+ * the current input buffer is not full.
+ */
+volatile char *snnap_writebuf();
+
+/* Invoke the NPU on the most recently written NPU. Move to the next input
+ * buffer. Precondition: the current write buffer is full but has not been
+ * invoked yet.
+ */
+void snnap_sendbuf();
+
+#endif
